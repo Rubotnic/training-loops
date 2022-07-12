bb71db9c444b384e8dc6640c3700ae9bc135cf5c
Rubotnic <info@rubotnic.com>
2022-07-12 13:39:08 +0200

    My loop training.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean javaDeveloper = true;
+
+        while(javaDeveloper) {
+            System.out.println("How many months do you need to train coding Java to be a Java programmer?");
+            int months = scan.nextInt();
+
+            if (months >= 80 && months <= 90) {
+                System.out.println("O my god, hurry up man!");
+            }
+
+            if (months >= 100) {
+                System.out.println("Yes, after 100 months your are a good java programmer.");
+            }
+            else {
+                System.out.println("You are still a rookie.");
+                javaDeveloper = true;
+
+            }
+        }
+    }
+}
